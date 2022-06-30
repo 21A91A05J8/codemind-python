b55499980f3b268ef9b19eb470cd7697db81c258
21A91A05J8 <97426310+21A91A05J8@users.noreply.github.com>
2022-07-01 03:47:44 +0530

    n=int(input()) for i in range(n):     x=i     y=1     for j in range(i+n):         if j>=n-i-1:             if j<n-1:                 print(x,end="")                 x-=1             if j==n-1:                 print("0",end="")             if j>n-1:                 print(y,end="")                 y+=1         else:             print(" ",end="")     print()

--- print_the_following_pattern.py
+++ print_the_following_pattern.py
@@ -1,8 +1,18 @@
 n=int(input())
 for i in range(n):
+    x=i
+    y=1
     for j in range(i+n):
         if j>=n-i-1:
-            print(i+1,end="")
+            if j<n-1:
+                print(x,end="")
+                x-=1
+            if j==n-1:
+                print("0",end="")
+            if j>n-1:
+                print(y,end="")
+                y+=1
         else:
             print(" ",end="")
     print()
+                
